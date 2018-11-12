6ef0727abef8442986345049aadd48a6109968da
yitzhak Matias <matias_6@hotmail.com>
2018-11-11 22:14:40 -0400

    -labo

---
+++ src/Quiz.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+import data from './quiz_data'
+
+class Quiz extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {quiz_position: 1};
+    }
+
+    render() {
+        return (
+            <div className="QuizQuestion">{data.quiz_questions[0].instruction_text}</div>
+        )
+    };
+
+}
+
+export default Quiz;
