8a4d7f355d1523cad333a8ba95efb99ebe7854d1
yitzhak Matias <matias_6@hotmail.com>
2018-11-11 23:54:19 -0400

    -labo0 this is a quizz data

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -6,7 +6,7 @@ class QuizQuestion extends Component {
         return (
             <main>
                 <section>
-                    <p> {this.props.quiz_question.instruction_text} </p>
+                    <p>{this.props.quiz_question.instruction_text}</p>
                 </section>
                 <section className="buttons">
                     <ul>
