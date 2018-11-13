0d4bbda65f59f73300e6e0d2eb46d50d6a6fcd84
yitzhak Matias <matias_6@hotmail.com>
2018-11-12 23:04:51 -0400

    -labo0 this is a quizz data

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,7 +1,10 @@
 import React, {Component} from 'react'
 import data from './quiz_data'
 import QuizQuestion from "./QuizQuestion";
+import QuizEnd from "./QuizEnd";
+
 let quizData = require('./quiz_data.json')
+
 class Quiz extends Component {
     constructor(props) {
         super(props)
@@ -9,9 +12,16 @@ class Quiz extends Component {
     }
 
     render() {
+
+        const isQuized = ((this.state.quiz_position) === quizData.quiz_questions.length);
+
         return (
-          //  <div className="QuizQuestion">{data.quiz_questions[0].instruction_text}</div>
-            <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}/>
+            //  <div className="QuizQuestion">{data.quiz_questions[0].instruction_text}</div>
+            <div>
+                {isQuized ? <QuizEnd/> :
+                    <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>}
+            </div>
+
         )
     };
 
